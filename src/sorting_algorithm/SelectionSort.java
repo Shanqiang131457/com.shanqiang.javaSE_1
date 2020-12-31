e4b39b529a4c80a37cee8ca4dec480971a939ac3
shanqiang <shanqiang1@sunline.cn>
2020-12-31 16:35:35 +0800

    Merge remote-tracking branch 'origin/master'

--- src/sorting_algorithm/SelectionSort.java
+++ src/sorting_algorithm/SelectionSort.java
@@ -5,18 +5,22 @@ import com.sun.scenario.effect.impl.sw.java.JSWColorAdjustPeer;
 public class SelectionSort {
     public static void main(String[] args) {
         int arr[]={2,13,4,5,1,10,9,76,22};
+        selectSort1(arr);
+    }
+    public static void selectSort(int arr[]){
 
-        for(int i=0;i<arr.length-1;i++){  //-1防止角标越界
+    }
+    public static void selectSort1(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
             int minPos=i;
             for(int j=i+1;j<arr.length;j++){
-                minPos = arr[j]<arr[minPos] ? j : minPos;
+                minPos=arr[j]<arr[minPos] ? j:minPos;
             }
-            System.out.println("minPos::"+minPos);
             swap(arr,i,minPos);
-            System.out.println("经过第"+i+"次循环之后数组的内容：");
-            printArr(arr);
         }
+        printArr(arr);
     }
+
     public static void printArr(int arr[]){
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+"\t");
@@ -28,3 +32,17 @@ public class SelectionSort {
         arr[j]=temp;
     }
 }
+
+        /*
+        for(int i=0;i<arr.length-1;i++){  //-1防止角标越界
+            int minPos=i;
+            for(int j=i+1;j<arr.length;j++){
+                minPos = arr[j]<arr[minPos] ? j : minPos;
+            }
+            System.out.println("minPos::"+minPos);
+            swap(arr,i,minPos);
+            System.out.println("经过第"+i+"次循环之后数组的内容：");
+            printArr(arr);
+        }
+
+         */
